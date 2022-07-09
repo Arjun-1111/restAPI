5e57e3544a6a917069b81f85b745c45c8b750d42
Arjun-1111 <contactme.xyz1@gmail.com>
2022-07-09 22:16:05 +0530

    minor change in route

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
 /* eslint-disable import/no-duplicates */
 
 import express from 'express';
 import { createUserHandler } from '../src/controller/user.controller';
+import validateResource from './middleware/validateResource';
+import { userSchema } from './schema/user.schema';
 
 const router = express.Router();
 
-router.post('/api/users', createUserHandler);
+router.post('/api/users', validateResource(userSchema), createUserHandler);
 
 export default router;
